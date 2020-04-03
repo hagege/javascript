b99fbd76626330743b101822933b004015706b4c
hagege <webmaster@aachenerkinder.de>
2020-04-03 22:41:51 +0200

    weitere Javascript-Dateien

---
+++ 06-plus.js
@@ -0,0 +1,15 @@
+// alte Notation soll nicht mehr gültig sein
+"use strict"
+
+// console.log (5 + 6);
+// console.log ("5" + "6");
+
+let water = 1.9
+
+let amount = 10 + water;
+console.log(amount);
+
+// console.log("Hallo, " + "Javascript");
+
+// console.log(true + false);
+
