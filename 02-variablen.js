81856093884bdda0a8a63019c0fc21d02d2c0c5c
hagege <webmaster@aachenerkinder.de>
2020-04-03 21:35:10 +0200

    erste Javascript-Dateien

---
+++ 02-variablen.js
@@ -0,0 +1,11 @@
+// Variable greeting definiert
+let greeting
+
+// der Variablen greeting einen Wert zuweisen
+greeting = "Hallo Welt!";
+
+let greeting2 = "Hallo Welt!";
+
+// Die Variable greeting wird 2 mal ausgegeben
+console.log(greeting2);
+console.log(greeting2);
---
+++ 03-variablen_teil_2.js
@@ -0,0 +1,13 @@
+// Variable greeting definiert
+
+let greeting = "Hallo Welt!";
+
+// Die Variable greeting wird 2 mal ausgegeben
+console.log(greeting);
+console.log(greeting);
+
+greeting = "Hallo Javascript!";
+
+// Die Variable greeting wird 2 mal ausgegeben
+console.log(greeting);
+console.log(greeting);
