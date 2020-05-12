6ab07c742f69b9fc935feeeaddfea2ddcb830a8c
hgg <postmaster@aachenerkinder.de>
2020-05-12 19:04:35 +0200

    Anhängen von todos funktioniert

--- 15 - Projekt Todo-Liste/Teil 00 (Ausgangsbasis)/js/app.js
+++ 15 - Projekt Todo-Liste/Teil 00 (Ausgangsbasis)/js/app.js
@@ -3,15 +3,15 @@ const KEY_ENTER = 13
 
 document.addEventListener("DOMContentLoaded", () => {
     // hier fehlt noch die richtige Klasse (bei Klick auf ENTER):
-    const addToDoButton = document.getElementById("add-student")
+    // const addToDoButton = document.getElementById("add-student")
     // const fieldInput = document.querySelectorAll("[class='new-todo']")
     const fieldInputs = document.getElementsByClassName("new-todo")
             // über "X" wird das jeweilige Element gelöscht.
     const closeButtons = document.querySelectorAll("[class='destroy']")
+    
     // das funktioniert schon (Remove Todo):  
     for (const closeButton of closeButtons) {
           closeButton.addEventListener("click", () => {
-            // console.log("Closebuttons: ", closeButtons)
             // besser das hier (weil das (Eltern-)Element komplett gelöscht wird):
             closeButton.parentElement.remove()
           })
@@ -20,60 +20,46 @@ document.addEventListener("DOMContentLoaded", () => {
     for (const fieldInput of fieldInputs) {
       fieldInput.addEventListener("keypress", (event) => {
         // console.log("fieldInputs: ", fieldInputs)
-        console.log(event)
+        // console.log(event)
         if (event.keyCode === KEY_ENTER) {
-          alert("Enter wurde gedrückt!")
+          // alert("Enter wurde gedrückt!")
           // Erst das Listenelement komplett erstellen
           // Listenelement erstellen (<li>):
+          console.log(fieldInput.value)
           const liElement = document.createElement("li")
-          // liElement.appendChild(strongElement)
+          const divElement = document.createElement("div")
+          const InputElement = document.createElement("input")
+          const LabelElement = document.createElement("label")
+          const ButtonElement = document.createElement("button")
+          const strongElement = document.createElement("strong")
+          liElement.appendChild(strongElement)
+          const ulElement = document.querySelector("ul.todo-list")
+          // dem div eine Klasse zuordnen:
+          divElement.classList.add("view")
+          InputElement.classList.add("toggle")
+          console.log("InputElement:", InputElement.checked)
+          if (InputElement.checked === false){
+            InputElement.checked = true
+          }
+          // ("checkbox")
+          ButtonElement.classList.add("destroy")
+          
+          // Elemente dem liElement zuordnen
+          liElement.appendChild(divElement)
+          liElement.appendChild(InputElement)
+          liElement.appendChild(LabelElement)
+          liElement.appendChild(ButtonElement)
+          LabelElement.appendChild(document.createTextNode(fieldInput.value))
+          
           // ul-Gruppe der Todo-Liste
-          liElement.classList.add("completed")
-          /*
-          const ulElement = document.querySelector("todo-list")
+          // liElement.classList.add("completed")
+          
+          // strongElement.appendChild(document.createTextNode("Hallo Welt"))
+          
           ulElement.appendChild(liElement)
-          */
+        
+          
         }
       })
-}
-
-    // klappt nicht (hier ist der Fehler: 0)
-    // auf ENTER soll die neue ToDo-Karte angehängt werden
-    /*
-    addToDoButton.addEventListener("click", (event) => {
-            event.preventDefault()
-              
-            const text = fieldInput.value
-        
-            if (text === "") {
-              return
-            }
-            
-            // Erst das Listenelement komplett erstellen
-            // Listenelement erstellen (<li>):
-                        const liElement = document.createElement("li")
-            liElement.appendChild(strongElement)
-            // ul-Gruppe der Todo-Liste
-            liElement.classList.add("todo-list")
-            const ulElement = document.querySelector("todo-list")
-            ulElement.appendChild(liElement)
-        
-            fieldInput.value = ""
-          })
-        
-
-    if (text === "") {
-      return
-    }
-
-    // Nach der Eingabe und Enter soll eine neue ToDo erstellt werden
-    // das hier ist zur Abfrage, wenn ENTER gedrückt wurde
-    fieldInput.addEventListener("keydown", (event) => {
-        console.log(event)
-        
-        if (event.keyCode === KEY_ENTER) {
-        alert("Enter wurde gedrückt!")
-        }
-    })
-*/
+  }
 });
