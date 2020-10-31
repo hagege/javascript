fdbd00a4f773f244294fb7c9d732564e8d77087b
hgg <postmaster@aachenerkinder.de>
2020-10-31 19:01:50 +0100

    neue und modifizierte Dateien

--- 02 - Erste Schritte/02-variablen.js
+++ 02 - Erste Schritte/02-variablen.js
@@ -1,11 +1,11 @@
 // Variable greeting definiert
-let greeting
+let greeting;
 
 // der Variablen greeting einen Wert zuweisen
 greeting = "Hallo Welt!";
 
-let greeting2 = "Hallo Welt!";
+let greeting2 = "Hallo Welt und hgg!";
 
 // Die Variable greeting wird 2 mal ausgegeben
-console.log(greeting2);
+console.log(greeting);
 console.log(greeting2);
